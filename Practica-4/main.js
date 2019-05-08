1f9332923ee70e615cd08ad1b37de00d9e0758c2
Pedro <pedromtorres22@gmail.com>
2019-05-08 17:33:12 +0200

    Practica 4: hecha

---
+++ Practica-4/main.js
@@ -0,0 +1,47 @@
+var s = document.getElementById("segundos"),
+    m = document.getElementById("minutos"),
+    h = document.getElementById("horas");
+
+var segundos = 0,
+    minutos = 0,
+    horas = 0;
+
+setInterval(function () {
+  segundos++;
+  switch (segundos) {
+    case 60:
+      segundos = 0;
+      minutos++;
+      switch (minutos) {
+        case 60:
+          minutos = 0;
+          horas++;
+          break;
+        default:
+          //
+      }
+      break;
+    default:
+      //
+  }
+  s.innerHTML = segundos;
+  m.innerHTML = minutos;
+  h.innerHTML = horas;
+},1000);
+
+var v1 = document.getElementById("v1"),
+    v2 = document.getElementById("v2"),
+    v3 = document.getElementById("v3"),
+    v4 = document.getElementById("v4");
+
+v1.onmouseon = function () {v1.muted = false};
+v2.onmouseon = function () {v2.muted = false};
+v3.onmouseon = function () {v3.muted = false};
+
+v1.onmouseover = function () {v1.muted = true};
+v2.onmouseover = function () {v2.muted = true};
+v3.onmouseover = function () {v3.muted = true};
+
+var change = function (n) {
+  v4.src = "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente" + n.toString() +".mp4";
+}
